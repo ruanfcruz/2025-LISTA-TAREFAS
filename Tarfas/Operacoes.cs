e78fdfd0a518cba66681baa40d4a5b612c21268b
ruanfcruz <ruanfcruz01@gmail.com>
2025-10-20 23:11:44 +0000

    feat:

---
+++ Tarfas/Operacoes.cs
@@ -0,0 +1,47 @@
+using MySql.Data.MySqlClient;
+
+public class Operacoes
+{
+    private string connectionString = 
+    @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;password=S3nh4!F0rt3;database=user_poo;";
+    public int Criar(Tarefa tarefa)
+    {
+        using(var conexao = new MySqlConnection(connectionString))
+        {
+            conexao.Open();
+            string sql = @"INSERT INTO tarefa (nome, descricao, dataCriacao, status, dataExecucao) 
+                           VALUES (@nome, @descricao, @dataCriacao, @status, @dataExecucao);
+                           SELECT LAST_INSERT_ID();";
+            using (var cmd = new MySqlCommand(sql, conexao))
+            {
+                cmd.Parameters.AddWithValue("@nome", tarefa.Nome);
+                cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
+                cmd.Parameters.AddWithValue("@dataCriacao", tarefa.DataCriacao);
+                cmd.Parameters.AddWithValue("@status", tarefa.Status);
+                cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao);
+
+                return Convert.ToInt32(cmd.ExecuteScalar());
+            }
+        }
+    }
+
+    public Tarefa Buscar(int id)
+    {
+        return null;
+    }
+
+    public IList<Tarefa> Listar()
+    {
+        return Array.Empty<Tarefa>();
+    }
+
+    public void Alterar(Tarefa tarefa)
+    {
+
+    }
+
+    public void Excluir(int id)
+    {
+
+    }
+}
